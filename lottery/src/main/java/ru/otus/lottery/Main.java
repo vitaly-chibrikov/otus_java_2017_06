f882f2be01488c06f539696c3ca789ab30c87944
vitaly chibrikov <vitaly.chibrikov@gmail.com>
2017-05-25 19:17:58 +0300

    lottery

---
+++ lottery/src/main/java/ru/otus/lottery/Main.java
@@ -0,0 +1,22 @@
+package ru.otus.lottery;
+
+import java.io.IOException;
+
+/**
+ * Created by tully.
+ */
+public class Main {
+    private static final int MAX_WINNERS_COUNT = 3;
+
+    public static void main(String[] args) throws IOException {
+        String pathToFile = "emails.csv";
+
+        String seedString = "May the Force be with you";
+
+        new Lottery(
+                new EmailsReader(pathToFile),
+                new LotteryMachine(MAX_WINNERS_COUNT),
+                seedString
+        ).run();
+    }
+}
