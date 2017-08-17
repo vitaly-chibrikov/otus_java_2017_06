dbf62a544ce4f700af68ec43792005b73cbfcb03
Vitaly Chibrikov <vitaly.chibrikov@gmail.com>
2017-08-17 14:04:37 +0300

    Connection pools, OneToMany

---
+++ L10.2.3/src/main/java/ru/otus/main/Main.java
@@ -0,0 +1,35 @@
+package ru.otus.main;
+
+import java.beans.PropertyVetoException;
+import java.lang.management.ManagementFactory;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws PropertyVetoException, SQLException, InterruptedException {
+        //DataSourceFactory factory = new C3P0DataSourceFactory();
+        //DataSourceFactory factory = new DBCPDataSourceFactory();
+        DataSourceFactory factory = new HikariCPDataSourceFactory();
+
+        Connection connection = factory.get().getConnection();
+        System.out.println("DatabaseProductName: " + connection.getMetaData().getDatabaseProductName());
+
+        Connection connection2 = factory.get().getConnection();
+        System.out.println("DriverName: " + connection2.getMetaData().getDriverName());
+
+        Connection connection3 = factory.get().getConnection();
+        System.out.println("URL: " + connection3.getMetaData().getURL());
+
+        hold();
+    }
+
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    private static void hold() throws InterruptedException {
+        System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
+        while (true) {
+            Thread.sleep(1000);
+        }
+    }
+
+}
