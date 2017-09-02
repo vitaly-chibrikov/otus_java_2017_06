3d44c88a57544c773b2557ecc1cf6212d6c8fef5
Vitaly Chibrikov <vitaly.chibrikov@gmail.com>
2017-09-02 09:13:14 +0300

    resource example

--- L13.1.1/src/main/java/ru/otus/servlet/Main.java
+++ L13.1.1/src/main/java/ru/otus/servlet/Main.java
@@ -7,7 +7,12 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.stream.Collectors;
 
 /**
  * @author v.chibrikov
@@ -17,12 +22,13 @@ public class Main {
     private final static String PUBLIC_HTML = "/static";
 
     public static void main(String[] args) throws Exception {
+        new Main().start();
+    }
+
+    private void start() throws Exception {
+        resourcesExample();
 
         ResourceHandler resourceHandler = new ResourceHandler();
-
-        URL url = Main.class.getResource(PUBLIC_HTML + "/index.html");
-        System.out.println(url);
-
         Resource resource = Resource.newClassPathResource(PUBLIC_HTML);
         resourceHandler.setBaseResource(resource);
 
@@ -35,5 +41,16 @@ public class Main {
 
         server.start();
         server.join();
+    }
+
+    private void resourcesExample() {
+        URL url = Main.class.getResource(PUBLIC_HTML + "/index.html"); //local path starts with '/'
+        System.out.println(url);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            System.out.println(br.readLine());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
